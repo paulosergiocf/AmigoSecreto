d6edf4d459dd741b9b7ecf715be9e871f50f875a
Paulo Sergio <paulosergiocf.dev@gmail.com>
2023-11-29 01:04:58 -0300

    ENV: configurações para deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn setup.wsgi --log-file -
+release: python manage.py migrate
+web: gunicorn setup.wsgi
--- setup/settings.py
+++ setup/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
